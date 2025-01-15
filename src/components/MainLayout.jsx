5fe486e614a7e47c9c4659bb8816a1dd815fb454
Majd Assad <majdassad123@gmail.com>
2025-01-15 14:13:27 -0500

    Added animation to main menu and improved navigation

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,19 +1,26 @@
 // React
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useLayoutEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 // Components
 import Footer from "./Footer";
 import Header from "./Header";
 
 const MainLayout = () => {
+  const location = useLocation();
+
+  // scroll to top of page after a page transition.
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [location.pathname]);
+
   return (
     <div className="bg-background-100">
       <Header />
       <main className="min-h-screen">
         <Outlet />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,20 +3,35 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 // Style & CSS
-import { HiOutlineMenu } from "react-icons/hi";
-import { useSpring, animated, easings } from "@react-spring/web";
+import { HiOutlineMenu, HiOutlineX } from "react-icons/hi";
+import { useSpring, animated, easings, useTrail } from "@react-spring/web";
 
 const Menu = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const navigate = useNavigate();
 
+  const navigate = useNavigate();
+  const menuItems = [
+    { name: "Home", url: "/" },
+    { name: "About", url: "/about" },
+    { name: "Projects", url: "/projects" },
+    { name: "Contact", url: "/contact" },
+  ];
+
+  const trail = useTrail(menuItems.length, {
+    config: { mass: 1, tension: 2000, friction: 200 },
+    delay: 600,
+    opacity: menuOpen ? 1 : 0,
+    x: menuOpen ? 0 : 50,
+    from: { opacity: 0, x: 50 },
+  });
   const menuAnimation = useSpring({
     transform: menuOpen ? "translateX(0%)" : "translateX(100%)",
     config: {
-      duration: 500, // Adjust duration
-      easing: easings.easeInOutQuad, // Use a smooth easing curve
+      duration: 800, // Adjust duration
+      easing: easings.easeInOutQuart, // Use a smooth easing curve
     },
   });
+
   // Effect to toggle `overflow-hidden` on the <body>
   useEffect(() => {
     if (menuOpen) {
@@ -38,7 +53,7 @@ const Menu = () => {
         onClick={() => setMenuOpen(!menuOpen)}
         className="fixed top-4 right-4 z-50"
       >
-        <HiOutlineMenu size={40} />
+        {menuOpen ? <HiOutlineX size={40} /> : <HiOutlineMenu size={40} />}
       </button>
 
       {/* Full-Screen Menu */}
@@ -51,38 +66,16 @@ const Menu = () => {
         className={"fixed inset-0 bg-black flex justify-center items-center"}
       >
         <ul className="text-white text-center space-y-6">
-          <li>
-            <a
-              onClick={() => navigateTo("/")}
-              className="cursor-pointer text-5xl hover:text-blue-400"
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              onClick={() => navigateTo("/about")}
-              className="cursor-pointer text-5xl hover:text-blue-400"
-            >
-              About
-            </a>
-          </li>
-          <li>
-            <a
-              onClick={() => navigateTo("/projects")}
-              className="cursor-pointer text-5xl hover:text-blue-400"
-            >
-              Projects
-            </a>
-          </li>
-          <li>
-            <a
-              onClick={() => navigateTo("/contact")}
-              className="cursor-pointer text-5xl hover:text-blue-400"
-            >
-              Contact
-            </a>
-          </li>
+          {trail.map((props, index) => (
+            <animated.li key={index} style={props}>
+              <a
+                onClick={() => navigateTo(menuItems[index].url)}
+                className={`cursor-pointer text-5xl hover:text-blue-400`}
+              >
+                {menuItems[index].name}
+              </a>
+            </animated.li>
+          ))}
         </ul>
       </animated.div>
     </div>
